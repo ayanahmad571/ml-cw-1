afd121cef7aadb1f29581e39d87e978ad6088c6f
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2022-02-22 01:14:38 +0000

    KNN implemented

--- classifier.py
+++ classifier.py
@@ -1,26 +1,34 @@
 from random import shuffle
 import numpy
 import math
+from scipy.stats import mode
+from sklearn.neighbors import KNeighborsClassifier
+from collections import Counter
+
 # classifier.py
 # Lin Li/26-dec-2021
 #
 # Use the skeleton below for the classifier and insert your code here.
+#Euclidean Distance
+def euclidean_distance(x1, x2):
+    return numpy.sqrt(numpy.sum((x1 - x2) ** 2))
+
+# https://stackoverflow.com/questions/4601373/better-way-to-shuffle-two-numpy-arrays-in-unison
+def shuffle_array(a, b):
+    a = numpy.asarray(a)
+    b = numpy.asarray(b)
+    assert len(a) == len(b)
+    p = numpy.random.permutation(len(a))
+    return a[p], b[p]
 
 class Classifier:
     def __init__(self):
         print("INIT-CLASS", self)
         self.k = 3
-    
-    # https://stackoverflow.com/questions/4601373/better-way-to-shuffle-two-numpy-arrays-in-unison
-    def shuffle_array(self, a, b):
-        a = numpy.asarray(a)
-        b = numpy.asarray(b)
-        assert len(a) == len(b)
-        p = numpy.random.permutation(len(a))
-        return a[p], b[p]
-        
+   
+       
     def train_test_split(self, X, y, split):
-        shuffle_X, shuffle_y = self.shuffle_array(X,y) 
+        shuffle_X, shuffle_y = shuffle_array(X,y) 
         X_train = list()
         X_test = list()
         y_train = list()
@@ -45,12 +53,22 @@ class Classifier:
     
     def fit(self, data, target):
         print("FIT", data, target)
+        self.X = data
+        self.y = target
         self.X_train, self.X_test, self.y_train, self.y_test = self.train_test_split(data, target, 0.8)
-        print("x-train", self.X_train)
-        print("x-test", self.X_test)
-        print("y_train", self.y_train)
-        print("y_test", self.y_test)
         
+       
     def predict(self, data, legal=None):
-        return 1
-        
+        y_pred = list()
+        for x in data:
+            # Compute distances between x and all examples in the training set
+            distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+            # Sort by distance and return indices of the first k neighbors
+            k_idx = numpy.argsort(distances)[: self.k]
+            # Extract the labels of the k nearest neighbor training samples
+            k_neighbor_labels = [self.y_train[i] for i in k_idx]
+            # return the most common class label
+            most_common = Counter(k_neighbor_labels).most_common(1)
+            y_pred.append(most_common[0][0])
+        print(numpy.array(y_pred))
+        return y_pred[0]
